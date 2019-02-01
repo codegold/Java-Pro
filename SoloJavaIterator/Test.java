a2262592da39ddce64d26c6c94d2483d3fe77773
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-02-01 14:04:43 +0300

    Iterator (updates) Iterator<Integer> itnum = numbers.iterator()

--- SoloJavaIterator/Test.java
+++ SoloJavaIterator/Test.java
@@ -4,15 +4,35 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Test {
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
         LinkedList<String> animals = new LinkedList<>();
         animals.add("fox");
         animals.add("cat");
         animals.add("dog");
         animals.add("rabbit");
+        System.out.println();
+
+        LinkedList<Integer> numbers = new LinkedList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        Iterator<Integer> itnum = numbers.iterator();
+        while(itnum.hasNext()) {
+            int valnumber = itnum.next();
+            System.out.println(valnumber);
+        }
+        System.out.println();
 
         Iterator<String> it = animals.iterator();
-        String value = it.next();
-        System.out.println(value);
+        /**Iterator is class name, java recommendation is
+          class name always start with capital letter,but
+          with small "i" is the name of the method
+        */
+         while (it.hasNext()) {
+            String value = it.next();
+            System.out.println(value);
+        }
     }
 }
